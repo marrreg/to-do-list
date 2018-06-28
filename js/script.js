fdbbb8ea3282b521c8e69678469bfb3ba66c36ea
kguzik <karolina.guzik08@gmail.com>
2018-06-28 21:52:06 +0200

    basic layout

--- js/script.js
+++ js/script.js
@@ -7,18 +7,31 @@ let deleteButtons = document.getElementsByClassName('deleteButton');
 //
 //});
 
+populateWithSampleTasks();
+
 newTaskButton.addEventListener('click', function () {
     addNewTask(newTaskText.value);
-
-    for (let i = 0; i < deleteButtons.length; i++) {
-        deleteButtons[i].addEventListener('click', function () {
-            this.parentNode.remove();
-        });
-    }
 });
 
 function addNewTask(taskText) {
-    let newTask = '<li>' + taskText + ' ' + '<button class="deleteButton">X</button>' + '</li>';
-    taskList.innerHTML += newTask;
-    newTaskText.value = '';
+    if (taskText.length) {
+        let newTask = '<li class="list-item">' + '<i class="fa fa-circle-thin completeButton" aria-hidden="true"></i>' + '<i class="fa fa-circle-thin startButton" aria-hidden="true"></i>' + taskText + ' ' + '<i class="fa fa-trash-o deleteButton" aria-hidden="true"></i>' + '</li>';
+        taskList.innerHTML += newTask;
+        newTaskText.value = '';
+        for (let i = 0; i < deleteButtons.length; i++) {
+            deleteButtons[i].addEventListener('click', function () {
+                this.parentNode.remove();
+            });
+        }
+    } else {
+        console.log('Rejected: Attempted to add empty task.');
+    }
+}
+
+function populateWithSampleTasks() {
+    addNewTask("Wyniesc smieci");
+    addNewTask("Zrobic zakupy");
+    addNewTask("Wyprowadzic psa");
+    addNewTask("Stowrzyc nowa aplikacje do zadan");
+    addNewTask("Ugotowac obiad");
 }
